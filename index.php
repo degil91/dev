988be49531273de6b14ef931ef5cb58cc0697e04
degil91 <christopertv77@gmail.com>
2024-05-04 15:20:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,189 @@
-tes
+
+<!doctype html>
+<html amp lang="id">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+<script async src="https://cdn.ampproject.org/v0.js"></script>
+<script async custom-element="amp-carousel" src="https://cdn.ampproject.org/v0/amp-carousel-0.1.js"></script>
+<script async custom-element="amp-sidebar" src="https://cdn.ampproject.org/v0/amp-sidebar-0.1.js"></script>
+<script async custom-element="amp-anim" src="https://cdn.ampproject.org/v0/amp-anim-0.1.js"></script>
+<script async custom-element="amp-iframe" src="https://cdn.ampproject.org/v0/amp-iframe-0.1.js"></script>
+<script async custom-element="amp-selector" src="https://cdn.ampproject.org/v0/amp-selector-0.1.js"></script>
+<script type="application/ld+json">
+  {
+    "@context": "http://schema.org",
+    "@type": "Game",
+    "name": "Tangansakti99",
+    "author": {
+       "@type": "Person",
+       "name": "Tangansakti99"
+        },
+     "headline": "Tangansakti99 >> Situs Slot Online Resmi Pragmatic Play & Pg Soft",
+  "description": "Tangansakti99, Adalah Situs Slot Online Terpercaya Dan Terbaik 2024, Menawarkan Berbagai Permainan Slot Yang Seru Dan Adil. Dengan Reputasi Yang Solid, Mereka Memberikan Pengalaman Taruhan Yang Memuaskan.",
+  "keywords": ["Tangansakti99, slot, slot gacor, pg soft, slot online, slot gacor hari ini, slot gacor hari ini, judi slot"],
+  "image": "https://i.ibb.co/4m6PkPW/8-logo.png",
+      "url": "https://0999994.com/",
+    "publisher": {
+        "@type": "Organization",
+        "name": "Tangansakti99"
+      },
+      "aggregateRating": {
+        "@type": "AggregateRating",
+        "ratingValue": "80",
+        "bestRating": "124",
+        "worstRating": "0",
+        "ratingCount": "777168"
+      },
+      "inLanguage": "id"
+    }
+</script>
+<title>Tangansakti99 >> Situs Slot Online Resmi Pragmatic Play & Pg Soft</title>
+<meta name="google-site-verification" content="KzlUWEBvqkkVsyT0pU1-vd6edNubz7sj7rBCivICRPA" />
+<meta name="keywords" content="Tangansakti99, slot, slot gacor, pg soft, slot online, slot gacor hari ini, slot gacor hari ini, judi slot"/>
+<meta name="description" content="Tangansakti99, Adalah Situs Slot Online Terpercaya Dan Terbaik 2024, Menawarkan Berbagai Permainan Slot Yang Seru Dan Adil. Dengan Reputasi Yang Solid, Mereka Memberikan Pengalaman Taruhan Yang Memuaskan."/>
+<link rel="canonical" href="####">
+<link href="https://i.ibb.co/4m6PkPW/8-logo.png" rel="shortcut icon" type="image/x-icon">
+<meta property="og:title" content="Tangansakti99 >> Situs Slot Online Resmi Pragmatic Play & Pg Soft"/>
+<meta property="og:description" content="Tangansakti99, Adalah Situs Slot Online Terpercaya Dan Terbaik 2024, Menawarkan Berbagai Permainan Slot Yang Seru Dan Adil. Dengan Reputasi Yang Solid, Mereka Memberikan Pengalaman Taruhan Yang Memuaskan."/>
+<meta property="og:url" content="####"/>
+<meta property="og:site_name" content="Tangansakti99"/>
+<meta property="og:author" content="Tangansakti99"/>
+<meta property="og:type" content="website"/>
+<meta property="og:image" content="https://i.ibb.co/4m6PkPW/8-logo.png">
+<meta name="robots" content="index, follow"/>
+<meta name="googlebot" content="index,follow">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="alternate" hreflang="id" href="####" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" type="text/css"/>
+
+<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
+
+<style amp-custom>
+body{font-family:'Roboto',sans-serif;background-color:#181717;font-size:14px;color:#262626;margin:0}a{text-decoration:none}#mob-header{position:fixed;z-index:20;width:100%;height:50px;background-color:#171342;box-shadow:0 1px 0 rgba(0,0,0,.3);top:0}#mob-top{text-align:center}#mob-header #mob-logo{display:inline-block;width:60%;height:50px}#mob-header #mob-logo amp-img{max-height:50px;margin-top:0px}#mob-header #mob-logo amp-img img{object-fit:contain}#mob-topl{margin:0;position:absolute;left:15px;top:15px;width:25px;height:20px}#mob-topr{margin:0;position:absolute;right:15px;top:50%;transform:translateY(-50%)}#mob-topr a{color:#fff}#mob-topr i{font-size:22px}.menu-hamburger{cursor:pointer;display:block;width:25px;height:20px}.menu-trigger [data-icon="menu"]{display:inline-block;position:absolute;left:50%;top:50%;bottom:auto;right:auto;transform:translate(-50%,-50%);width:25px;height:2px;transition:.5s ease-in-out;background-color:#fff}.menu-trigger [data-icon="menu"]::before,.menu-trigger [data-icon="menu"]::after{content:'';width:100%;height:100%;position:absolute;background-color:inherit;left:0}.menu-trigger [data-icon="menu"]::after{top:7px}.menu-trigger [data-icon="menu"]::before{bottom:7px}amp-sidebar{background-color:#fff;width:280px}amp-sidebar ul{margin:0;padding:0;list-style:none;display:block}amp-sidebar ul li{padding:12px 16px;border-bottom:1px solid #ddd}amp-sidebar ul li a{display:flex;align-items:center}amp-sidebar ul li a amp-img{width:21px;height:21px;margin-right:16px}amp-sidebar ul li a span{color:#292b2c;font-size:16px;text-transform:uppercase}amp-sidebar ul li:first-child{background-color:#1da0cb;border-bottom:0}amp-sidebar ul li:nth-child(2){background-color:#ba811e;border-bottom:0}amp-carousel{margin-top:50px}#mob-con{max-width:768px;margin:0 auto}#mob-footer{position:fixed;left:0;bottom:0;width:100%;text-align:center;background-color:#171342;font-size:0}#mob-footer div{display:inline-block;width:25%;font-size:14px}#mob-footer div.active,#mob-footer div:hover{background-color:#022558}#mob-footer i{display:block;font-size:28px;margin-bottom:3px}#mob-footer a{color:#fff;padding:8px 0;display:block}#mob-content{padding-bottom:80px}.game-list{margin:0 auto}.game-list__detail{display:flex;flex-wrap:wrap;padding:10px;justify-content:center}.game-list__detail .detail-list{position:relative;padding:1%;width:31.33%;font-size:0}.game-list__detail .detail-list a{color:#fff}.game-list__detail .detail-list img{max-width:100%;height:auto;border-radius:8px}.game-list__detail .detail-list__title{position:absolute;left:0;right:0;bottom:15px;text-align:center;font-size:16px}section.footer{padding:20px 0}section.footer .copyright{color:#aaa;font-size:16px;text-align:center}.slot88{padding:15px;border-top:1px solid rgba(113,123,143,0.2);color:#fff;font-size:16px;}.slot88 h1,.slot88 h2,.slot88 h3,.slot88 h4,.slot88 h5{margin:0 0 10px;line-height:normal}.con-lshm amp-iframe,.con-sggl-ps amp-iframe{max-height:220px;min-height:220px;background-color:#fff;}
+
+  /*** COLORS ***/
+  #mob-header {
+    background-color: #3b0731;
+  }
+  #mob-footer {background-color: #73095d}
+  #mob-footer a {color: #ffffff}
+
+  amp-sidebar {background-color: #430637}
+  amp-sidebar ul li, amp-sidebar ul li:first-child, amp-sidebar ul li:nth-child(2) {border-color: #73095d}
+  amp-sidebar ul li:first-child {background-color: #6a6a6a;}
+  amp-sidebar ul li a span {color: #ffffff}
+
+  amp-sidebar ul li a img {-webkit-filter: invert(100%); filter: invert(100%);}
+
+  .slot88 a {color: #73095d;}
+
+.link-container{display:flex;justify-content:center;font-size:18px;padding:0;width: 100%;}.link-container a{width:50%;text-align:center;padding:15px 20px;text-transform:uppercase}.login-button,.register-button{color:#fff}.register-button{background:#741a62;background:linear-gradient(to bottom,#73095d 0%,#421239 100%)}.register-button:hover{background:linear-gradient(to bottom,#421239 0%,#73095d 100%)}.login-button{background:linear-gradient(to bottom,#ba811e 0%,#644510 100%)}.login-button:hover{background:linear-gradient(to bottom,#644510 0%,#ba811e 100%)}
+</style>
+</head>
+
+<body>
+
+  <amp-sidebar id="sidebar" class="sidebar" layout="nodisplay" side="left">
+    <ul>
+      <li><a href="https://0999994.com/" target="_blank" rel="noopener"><amp-img width="21" height="21" layout="responsive" src="icon/login.svg" alt="Log in"></amp-img><span>Login</span></a></li>
+      <li><a href="https://0999994.com/" target="_blank" rel="noopener"><amp-img width="21" height="21" layout="responsive" src="icon/signup.svg" alt="Daftar"></amp-img><span>Daftar</span></a></li>
+      
+    </ul>
+  </amp-sidebar>
+
+  <div id="mob-header">
+
+    <div id="mob-top">
+      <div id="mob-logo"><a href="https://0999994.com/" target="_blank" rel="noopener"><amp-img layout="responsive" height="90" width="200" src="https://i.ibb.co/6HXg2kH/sbobet-tangansakti99-1.png" alt="Logo Tangansakti99" noloading></amp-img></a></div>
+      <div id="mob-topl">
+        <div class="menu-hamburger" role="button" tabindex=1 on="tap:sidebar"><label class="menu-trigger"><i data-icon="menu"></i></label></div>
+      </div>
+      <!--<div id="mob-topr"><a href="/"><i class="far fa-bell"></i></a></div>-->
+    </div>
+
+  </div>
+
+  <amp-carousel class="carousel-container" layout="responsive" width="1920" height="613" type="slides" autoplay delay="5000" loop>
+    <amp-img src="https://i.ibb.co/41kVc8B/Tangansakti99-slot-online-terpercaya.webp" width="1920" height="613" layout="responsive" alt="situs slot88"></amp-img>
+
+  </amp-carousel>
+
+  <div class="link-container container">
+    <a href="https://0999994.com/" target="_blank" rel="noopener" class="register-button">DAFTAR</a>
+    <a href="https://0999994.com/" target="_blank" rel="noopener" class="login-button">LOGIN</a>
+  </div>
+
+  <div id="mob-con">
+
+    <div id="mob-content">
+
+ 
+
+      <section class="slot88">
+        <div class="slot88-body">
+
+          <h1 style="text-align: left;"><span style="color: #800080;"><strong>Tangansakti99 >> Situs Slot Online Resmi Pragmatic Play & Pg Soft</strong></span></h1>
+<p style="text-align: justify;">Situs <a href="https://0999992.com/">Tangansakti99</a>&nbsp;merupakan salah satu platform terkemuka yang menawarkan beragam permainan judi slot online berkualitas. Jika Anda mencari kesempatan untuk meraih keuntungan besar dengan modal minim, Tangansakti99 adalah pilihan yang tepat. Daftarkan akun Anda sekarang di Tangansakti99, situs resmi judi slot gacor dan provider Pgsoft terpercaya. Nikmati pengalaman bermain slot online terbaik dari berbagai penyedia game yang akan membawa Anda menuju kemenangan.</p>
+<p style="text-align: justify;">Kamu juga bisa membuktikan sendiri bahwasannya Tangansakti99 telah di kenal sebagai situs judi slot gacor terpercaya hal tersebut dapat dibuktikan dengan mudah ditemukannya di mesin pencarian google dan memiliki reputasi digital yang sangatlah baik.&nbsp;<a href="https://0999992.com/">Tangansakti99&nbsp;slot</a> juga memiliki lisensi resmi dan diawasi oleh otoritas yang berwenang dari berbagai negara hingga mampu menjadi tempat bermain slot gacor dengan aman dan nyaman. Kami sebagai situs resmi menjamin privasi data member serta mampu menyediakan sistem permainan yang fair play.</p>
+<h2>8 Permainan Slot Gacor Maxwin Dari Platform Pragmatic Play</h2>
+<p style="text-align: justify;">Pragmatic play merupakan salah satu penyedia game perangkat lunak casino yang menyediakan berbagai pilihan game slot online gacor hari ini yang menjadi favorit oleh pemain&nbsp;<a href="https://0999992.com/">slot gacor hari ini</a>. Berikut Ini merupakan hasil survey yang telah didapatkan, pecinta slot slot gacor indonesia dengan game slot dengan platform terbaik pragmatic Play diantaranya :</p>
+<ol style="text-align: justify;">
+<li style="text-align: left;"><strong>Slot Gacor Gate Of Olympus</strong></li>
+Gates Of Olympus merupakan permainan mesin slot gacor rtp live hari ini yang mengangkat bertema dewa yunani kuno zaman dulu, Zeus. Memiliki daya tarik permainan ini slot gacor yang sangat besar yang sangat mudah menang dan cukup mudah untuk mendapatkan simbol scatter untuk memasuki babak bonus dalam putaran gratis spin.
+<li style="text-align: left;"><strong>Slot Gacor Gate Of Gatot Kaca</strong></li>
+Gate of Gatot Kaca merupakan suatu permainan baru dari game slot gacor yang sedang populer saat ini dan saat ini sering sekali dimainkan oleh banyak pecinta pemain slot karena sering memberikan pengalaman bermain yang sering memberikan keuntungan.
+<li style="text-align: left;"><strong>Slot Gacor Sweet Bonanza</strong></li>
+Sweet Bonanza adalah salah satu game slot online resmi sudah sering dimainkan, akan tetapi ketenaran game slot gacor satu ini tidak memiliki penurunan karena kualitas dari game tersebut terpercaya dan terjakga hingga sampai saat ini.
+<li style="text-align: left;"><strong>Slot Gacor Mahjong Wins</strong></li>
+Provider pragmatic tidak ada hentinya mengembangkan game slot online, permainan satu ini sudah rilis sejak didirikannya provider slot online ini, tetapi tidak bisa dipungkiri hingga saat ini games slot gacor ini semakin tenar dan viral di kallangan masyarakat.
+<li style="text-align: left;"><strong>Slot Gacor Wisdom Of Athena</strong></li>
+Games Slot Wisdom Of Athena permainan dari pragmatic play yang sangat mudah dimainkan, semua kalangan sangat menggemari permainan dengan tema yang telah disediakan. Pemain pemula juga dapat emmenangkan game judi slot gacor ini dengan mudah.
+<li style="text-align: left;"><strong>Slot Gacor Aztec Gems</strong></li>
+Salah satu mesin link slot gacor hari ini saat ini bernama Aztec Gems slot memiliki tema dengan budaya Aztec. Tema terinspirasi dari peradaban Aztec kuno di Amerika Tengah.
+<li style="text-align: left;"><strong>Slot Gacor Joker Jewels</strong></li>
+Joker Jewels merupakan game slot hadir dengan tema yang sangat unik dipanding dengan tema permaianan slot online resmi lainnya, selain tampilan dan kualitas yang bagus permaianan satu ini mudah untuk dimenangkan.
+<li style="text-align: left;"><strong>Slot Gacor Wild West Gold</strong></li>
+Slot gacor wild west wild gold telah hadir kembali dengan perubahan yang signifikan, kualitas gambar game slot gacor hari ini sangat memanjakan mata setiap pemain. Sehingga para pemain slot online sangat betah dan nyaman saat bermain.</ol>
+<h2>Pilihan Rekomendasi Situs Slot Online Resmi 2024</h2>
+<p style="text-align: justify;">Saat memilih situs slot online kalian pasti pernah merasa bimbang, Tangansakti99 merekomendasikan situs slot online terbaik sangat terpercaya sehingga anda dapat bermain secara aman dan nyaman, kami hadir untuk menhapus rasa was was yang seldang kalian hadapi. Tangansakti99 merupakan situs slot online terbaik dipercaya kami melayani selama 24 jam nonstop. Jika anda mengalami kendala saat bermain kami akan membantu anda dengan professional.</p>
+<h3>Keuntungan Bermain Tangansakti99 Slot</h3>
+<p style="text-align: justify;">Setiap jenis situs game&nbsp;<a href="https://0999992.com/">slot online</a>&nbsp;biasanya memiliki tingkat pembayaran tertentu. Tidak terkecuali untuk Tangansakti99, di mana kami mengantisipasi bahwa setiap&nbsp;Rupiah yang Anda hasilkan dari taruhan akan menghasilkan yang lebih besar. Mesin slot dengan RTP Live 96% akan membayar untuk setiap kemenangan. Berikut ini adalah beberapa keuntungan bermain slot di situs terbaik selain menikmati game dengan grafis menarik dan efek suara yang mengesankan:</p>
+<ul>
+<li>Banyak bonus tambahan dari situs slot terpercaya.</li>
+<li>Terdapat pilihan tema game terbaru.</li>
+<li>Melayani permainan slot dengan deposit&nbsp;minimum.</li>
+<li>Proses WD dana mudah dan cepat.</li>
+<li>Pilihan Provider Slot&nbsp;terlengkap.</li>
+<li>Metode transaksi Lengakap&nbsp; transfer Bank, uang elektronik, hingga deposit pulsa</li>
+</ul>
+<h2>Cara Mudah Dan Cepat Daftar Slot Online Tangansakti99</h2>
+<p style="text-align: justify;">Jika anda ingin melakukan pendaftaran <a href="https://0999992.com/">Situs Slot Tangansakti99</a>&nbsp;kalian dapat melakukan dengan cepat dan mudah di situs slot Tangansakti99. Untuk melakukan registrasi sangat mudah&nbsp; sekali berikut ini kami akan membahas bagaimana cara melakuakan registrasi di situs Tangansakti99 sebagai berikut:</p>
+<ul>
+<li>Kunjungi halaman pendaftaran Tangansakti99</li>
+<li>Isi form pendaftaran dengan valid</li>
+<li>Login account yang telah anda buat</li>
+</ul>
+<p style="text-align: justify;">Jika anda memiliki kendala kalian dapat meminta bantuan kepada customer service Tangansakti99, kami akan siap membantu anda.</p>
+	   </div>
+	   
+      </section>
+      <section class="footer">
+        <div class="copyright">SeoDusun © Copyright 2024. All Right Reserved.</div>
+      </section>
+
+    </div>
+
+    <div id="mob-footer">
+      <div><a href="https://0999994.com/" target="_blank" rel="noopener"><i class="far fa-user-circle"></i> Login</a></div>
+      <div><a href="https://0999994.com/" target="_blank" rel="noopener"><i class="far fa-edit"></i> Daftar</a></div>
+      <div><a href="https://0999994.com/" target="_blank" rel="noopener"><i class="fab fa-whatsapp"></i> Whatsapp</a></div>
+      <div><a href="https://0999994.com/" target="_blank" rel="noopener"><i class="fas fa-headset"></i> Livechat</a></div>
+    </div>
+
+  </div>
+
+</body>
+</html>
